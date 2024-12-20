94c7ebceb033f623630bbe84b7d4bd772d091445
lowk <davidkevinit@gmail.com>
2024-12-20 15:03:14 +0700

    fix dependency

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ docker run \
 	-p 127.0.0.1:6666:80 \
 	-v "./config.json:/opt/gophish/config.json" \
 	-v "./gophish.db:/opt/gophish/gophish.db" \
+	-v "./export:/opt/gophish/export" \
   --name gophish-custom -d gophish:custom
