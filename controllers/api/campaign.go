b349c79054262595a452dd913d47191df8216081
lowk <davidkevinit@gmail.com>
2024-12-26 23:03:21 +0700

    send via queue to update status

--- controllers/api/campaign.go
+++ controllers/api/campaign.go
@@ -39,8 +39,8 @@ func (as *Server) Campaigns(w http.ResponseWriter, r *http.Request) {
 		// If the campaign is scheduled to launch immediately, send it to the worker.
 		// Otherwise, the worker will pick it up at the scheduled time
 		if c.Status == models.CampaignInProgress {
-			//go as.worker.LaunchCampaign(c)
-			log.Infof("Cancelled send mail by built-in, use the external send mailer instead of")
+			go as.worker.LaunchCampaign(c)
+
 		}
 		JSONResponse(w, c, http.StatusCreated)
 	}
